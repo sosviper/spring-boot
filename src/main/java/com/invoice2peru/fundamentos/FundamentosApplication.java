0d448311f864bd613916dfa4cf55b1af1680af1e
Victor Valencia <sosviperdigital@gmail.com>
2022-02-12 02:04:41 -0500

    first commit

---
+++ src/main/java/com/invoice2peru/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.invoice2peru.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invoice2peru/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoice2peru.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
